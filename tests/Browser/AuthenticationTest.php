59ef475336cdc577cd8d2c9d07693ce24c6a7868
Leonid Lezner <kontakt@lezner.de>
2021-08-06 15:05:10 +0200

    Working on mailhog related tests 1

--- tests/Browser/AuthenticationTest.php
+++ tests/Browser/AuthenticationTest.php
@@ -13,19 +13,18 @@ use Illuminate\Support\Facades\Mail;
 /**
  * @group auth
  */
-class AuthTest extends DuskTestCase
+class AuthenticationTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
-    /** @test */
-    public function a_user_can_login()
+    public function test_user_can_login()
     {
         $user = User::factory()->create([
             'email' => 'taylor@laravel.com',
         ]);
 
         $this->browse(function ($browser) use ($user) {
-            $browser->visit(route('login'))
+            $browser->visitRoute('login')
                     ->type('email', $user->email)
                     ->type('password', 'password')
                     ->press(__('auth.button_login'))
@@ -33,30 +32,40 @@ class AuthTest extends DuskTestCase
         });
     }
 
-    /** @test */
-    public function a_guest_can_register()
+    public function test_guest_can_register()
     {
         $password = 'myLongPassword555&&';
         
         $this->browse(function ($browser) use ($password) {
             
-            $browser->visit(route('register'))
+            $browser->visitRoute('register')
                     ->type('name', Str::random(10))
                     ->type('email', Str::random(10).'@gmail.com')
                     ->type('password', $password)
                     ->type('password_confirmation', $password)
                     ->press(__('auth.button_register'))
+                    ->assertRouteIs('verification.notice')
+                    ->assertSee(__('auth.button_resend_verification_email'));
+
+            $browser->visit('http://localhost:8025')
+                    ->waitForLocation('/')
+                    ->click('.messages > .msglist-message')
+                    ->withinFrame('#preview-html', function($browser) {
+                        $browser->waitForLink('Verify Email Address')
+                                ->clickLink('Verify Email Address');
+                    });
+
+            $browser->visit(RouteServiceProvider::HOME)
                     ->assertPathIs(RouteServiceProvider::HOME);
-            
+
             $browser->visit(route('logout'));
         });
     }
 
-    /** @test */
     public function a_guest_cannot_register_with_short_password()
     {
         $this->browse(function ($browser) {
-            $browser->visit(route('register'))
+            $browser->visitRoute('register')
                     ->type('name', Str::random(10))
                     ->type('email', Str::random(10).'@gmail.com')
                     ->type('password', "shorty")
@@ -64,6 +73,8 @@ class AuthTest extends DuskTestCase
                     ->press(__('auth.button_register'))
                     ->assertRouteIs('register')
                     ->assertSee('The password must be at least');
+            
+            $browser->visit(route('logout'));
         });
     }
 
