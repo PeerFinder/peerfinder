e757f2f4341eb995f86975e9d688956bb349835f
Leonid Lezner <kontakt@lezner.de>
2021-08-14 17:59:50 +0200

    Cleaning up the header

--- resources/views/components/ui/sections/header.blade.php
+++ resources/views/components/ui/sections/header.blade.php
@@ -1,6 +1,6 @@
 <header id="header" class="shadow bg-gray-800">
     <x-base.container class="px-3 flex justify-between py-2 items-center flex-row">
-        <div class="bg-yellow-200">
+        <div>
             <a href="{{ route('dashboard.index') }}" class="flex items-center">
                 <img src="{{ asset('/images/peerfinder_logo.png') }}" srcset="{{ asset('/images/peerfinder_logo@2x.png') }} 2x" class="w-7" />
                 <div class="text-2xl ml-2 text-gray-300">{{ config('app.name') }}</div>
@@ -8,7 +8,6 @@
         </div>
 
         <div class="flex items-center space-x-6 flex-row">
-
             <header-menu>
                 <template v-slot:trigger>
                     <span class="text-gray-300 hover:text-white"><x-ui.user.avatar :user="$user" size="40" class="rounded-full inline-block" /><x-ui.icon name="chevron" class="" /></span>
@@ -23,7 +22,6 @@
                     <a href="{{ route('logout') }}" class="block m-2 py-2 px-3 rounded-md text-center bg-gray-50 hover:bg-red-300 text-red-700 hover:text-red-800">{{ __('profile/user.menu_logout') }}</a>
                 </template>
             </header-menu>
-
         </div>
     </x-base.container>
 </header>
