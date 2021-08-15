5e597d40c2fb4a919ba15010a097993a1954af7c
Leonid Lezner <kontakt@lezner.de>
2021-08-15 23:21:05 +0200

    Fixing workflow and adding a small nova test

---
+++ tests/Browser/Nova/LoginTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Browser\Nova;
+
+use App\Models\Admin;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_can_render_admin_login()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(config('nova.path'))
+                    ->assertSee('Welcome Back!');
+        });
+    }
+
+    public function test_admin_can_login()
+    {
+        $admin = Admin::factory()->create();
+
+        $this->actingAs($admin)->browse(function (Browser $browser) use ($admin) {
+            $browser->visit(config('nova.path'))
+                    ->type('email', $admin->email)
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertSee('Dashboard');
+        });
+    }    
+}
