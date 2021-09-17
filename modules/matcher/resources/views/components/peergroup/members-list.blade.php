3e8316c217abdedf17fa8552a4314fadbcaecf4e
Leonid Lezner <kontakt@lezner.de>
2021-09-17 17:20:56 +0200

    Adding marker for required fields

--- modules/matcher/resources/views/components/peergroup/members-list.blade.php
+++ modules/matcher/resources/views/components/peergroup/members-list.blade.php
@@ -2,7 +2,7 @@
     @if ($pg && $pg->getMembers()->count() > 0)
     <div class="space-y-1 m-2">
         @foreach ($pg->memberships as $membership)
-        <div class="border p-2 rounded-md @if (auth()->id() == $membership->user_id) border border-pf-midorange @endif">
+        <div class="border p-2 rounded-md @if (auth()->id() == $membership->user_id) border-pf-midorange @endif">
             <x-matcher::ui.user :user="$membership->user" />
             @if ($membership->comment)
             <div class="bg-gray-50 text-sm mt-2 py-1 px-2 rounded-md">{{ $membership->comment }}</div>
