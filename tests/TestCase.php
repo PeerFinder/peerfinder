b3ba1a57c087c34f2c7f1dc7be964d2eec25b066
Leonid Lezner <kontakt@lezner.de>
2021-10-05 23:40:08 +0200

    Moving test env check to the creates application class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,13 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        if (!file_exists('.env.testing')) {
-            dd('Tests run only in testing environment! .env.testing is missing!');
-        }
-    }
 }
