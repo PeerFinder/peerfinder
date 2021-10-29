fa8513a747365978fe5808ded37d5cc14968552a
Leonid Lezner <kontakt@lezner.de>
2021-10-29 17:04:09 +0200

    Chaning pagination in conversations

--- modules/matcher/resources/views/partials/conversations.blade.php
+++ modules/matcher/resources/views/partials/conversations.blade.php
@@ -1,9 +1,7 @@
 @if ($conversations)
     <x-ui.card title="{{ __('matcher::peergroup.caption_conversations') }}">
     @forelse ($conversations as $conversation)
-        <div class="p-4">
-            {{ Talk::embedConversation($conversation) }}
-        </div>
+        {{ Talk::embedConversation($conversation) }}
     @empty
         <p class="text-center p-4">{{ __('matcher::peergroup.no_conversations_yet') }}</p>
     @endforelse
