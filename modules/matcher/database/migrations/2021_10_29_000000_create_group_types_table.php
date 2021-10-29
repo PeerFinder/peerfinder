6d78861a9b4405f523c9ee39665be11f41945aef
Leonid Lezner <kontakt@lezner.de>
2021-10-29 17:23:43 +0200

    Adding migration for the group type

---
+++ modules/matcher/database/migrations/2021_10_29_000000_create_group_types_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupTypesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('group_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_de');
+            $table->string('title_en');
+            $table->text('description_de');
+            $table->text('description_en');
+            $table->string('reference_de');
+            $table->string('reference_en');
+            $table->foreignId('group_type_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('group_types');
+    }
+}
