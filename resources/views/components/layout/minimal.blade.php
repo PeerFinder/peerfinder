42dac584bd1a27b8c64c9b9c587db5b28f1e3fba
Leonid Lezner <kontakt@lezner.de>
2022-02-06 12:57:03 +0100

    Adding submenu

--- resources/views/components/layout/minimal.blade.php
+++ resources/views/components/layout/minimal.blade.php
@@ -4,15 +4,16 @@
     <x-base.app>
         <x-ui.sections.header :user="$currentUser" />
 
+        @auth
         <div class="bg-white border-b mb-1">
             <x-base.container class="sm:px-3">
-                Submenu
+                <div class="flex justify-center align-middle">
+                    <x-ui.navigation.submenu-item route="dashboard.index">@lang('submenu.my_dashboard')</x-ui.navigation.submenu-item>
+                    <x-ui.navigation.submenu-item route="matcher.index">@lang('submenu.groups')</x-ui.navigation.submenu-item>
+                </div>
             </x-base.container>
         </div>
-
-        @isset($submenu)
-            
-        @endisset
+        @endauth
 
         <x-ui.status />
 
