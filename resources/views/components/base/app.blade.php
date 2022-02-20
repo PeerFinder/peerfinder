22ea18c866a2f96a9c0b21635cfedf997bf642f6
Leonid Lezner <kontakt@lezner.de>
2022-02-20 23:32:39 +0100

    Removing the loading time from app

--- resources/views/components/base/app.blade.php
+++ resources/views/components/base/app.blade.php
@@ -4,5 +4,4 @@
     </div>
     <script src="{{ mix('js/app.js') }}" defer></script>
     @include('cookie-consent::index')
-    <!-- {{ sprintf('%d', (microtime(true) - LARAVEL_START)*1000) }} -->
 </body>
