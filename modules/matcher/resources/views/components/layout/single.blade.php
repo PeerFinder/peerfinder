b89f445ffe2ce285dc884461e0f28056fcf5f77c
Leonid Lezner <kontakt@lezner.de>
2022-06-07 19:59:24 +0200

    Fixing user search layout

--- modules/matcher/resources/views/components/layout/single.blade.php
+++ modules/matcher/resources/views/components/layout/single.blade.php
@@ -39,7 +39,7 @@
             </div>
         </div>
         @else
-        <div class="sm:mt-10 mb-5 sm:mb-7">
+        <div class="mt-5 sm:mt-10 mb-5 sm:mb-7 px-4 sm:px-0">
             <a href="{{ $pg->getUrl() }}">
                 <x-ui.h1>
                     {{ $pg->title }}
--- resources/views/components/ui/forms/input.blade.php
+++ resources/views/components/ui/forms/input.blade.php
@@ -1,2 +1,4 @@
+@if (strlen($slot))
 <label for="{{ $attributes->get('name') }}" class="block mb-1 font-medium">{{ $slot }}@if($attributes->get('required'))<span class="text-red-500 ml-1">*</span>@endif @error($attributes->get('name')) <x-ui.icon name="exclamation" class="text-red-500" />@enderror</label>
+@endif
 <input {{ $attributes->merge(['class' => 'w-full border px-4 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-pf-midblue focus:border-transparent focus:bg-white'. ($errors->first($attributes->get('name')) ? ' bg-red-100 border-red-500': ' bg-gray-50 border-gray-300') ]) }} />
